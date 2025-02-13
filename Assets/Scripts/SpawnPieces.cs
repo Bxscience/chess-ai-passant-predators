86b6d7aa326de35ad613a6c50aeeef41631ad8dd
ElishaCahan <egordoncahan@gmail.com>
2025-02-13 13:03:53 -0500

    spacing figured out

--- Assets/Scripts/SpawnPieces.cs
+++ Assets/Scripts/SpawnPieces.cs
@@ -22,14 +22,16 @@ public class SpawnPieces : MonoBehaviour
     BlackQueen,
     BlackKing;
 
+    //BOARD: -19.25, -13.75, -8.25, -2.75, 2.75, 8.25, 13.75, 19.25
 
     void Start()
     {
-        //board is 0.4 by 0.4, centered at 0,0
+        //board is 38.5 x 38.5, centered at 0,0
         GameObject.Instantiate(Board, new Vector3(0,0,0), Quaternion.Euler(-90,0,0));
-        for (float i = 0; i < 0.46; i+=0.0575f)
+        for (float i = 0; i < 44; i+=5.5f)
         {
-            GameObject.Instantiate(WhitePawn, new Vector3(i-0.195f, 0.0175f, -0.125f), Quaternion.Euler(-90, 0, 0));
+            GameObject.Instantiate(WhitePawn, new Vector3(i-19.25f, 2, -19.5f), Quaternion.Euler(-90, 0, 0));
+            Debug.Log(i - 19.25f);
         }
         
     }
