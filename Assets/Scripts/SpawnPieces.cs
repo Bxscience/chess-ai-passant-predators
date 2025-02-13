abd0cbba46e00d8f6bb3511ea667cde79718bbcb
ElishaCahan <egordoncahan@gmail.com>
2025-02-13 13:16:53 -0500

    added piece arrays

--- Assets/Scripts/SpawnPieces.cs
+++ Assets/Scripts/SpawnPieces.cs
@@ -25,7 +25,9 @@ public class SpawnPieces : MonoBehaviour
     //BOARD: -19.25, -13.75, -8.25, -2.75, 2.75, 8.25, 13.75, 19.25, this is in both directions, x and z. Pieces should be at y=2
 
     GameObject[] WhitePawns = new GameObject[8];
+    GameObject[] WhitePieces = new GameObject[16];
     GameObject[] BlackPawns = new GameObject[8];
+    GameObject[] BlackPieces = new GameObject[16];
 
     void Start()
     {
@@ -35,7 +37,9 @@ public class SpawnPieces : MonoBehaviour
         for (float i = 0; i < 44; i+=5.5f)
         {
             WhitePawns[loopruns]= GameObject.Instantiate(WhitePawn, new Vector3(i-19.25f, 2, -13.75f), Quaternion.Euler(-90, 0, 0));
+            WhitePieces[loopruns] = WhitePawns[loopruns];
             BlackPawns[loopruns] = GameObject.Instantiate(BlackPawn, new Vector3(i - 19.25f, 2, 13.75f), Quaternion.Euler(-90, 0, 0));
+            BlackPieces[loopruns] = BlackPawns[loopruns];
             loopruns++;
         }
         
